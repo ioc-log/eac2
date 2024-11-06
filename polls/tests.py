4e05fc82200d7bae1ea9df6f8f65d19a5ba3087c
llorenç <csm1a.llorens.ortega@gmail.com>
2024-11-06 01:23:25 +0100

    configurant polls encara molts mes test varis

--- polls/tests.py
+++ polls/tests.py
@@ -57,7 +57,7 @@ class MySeleniumTests(StaticLiveServerTestCase):
         self.selenium.get('%s%s' % (self.live_server_url, '/admin/polls/question/add/'))
         self.selenium.find_element(By.NAME, "question_text").send_keys("Pregunta con 100 opciones")
         self.selenium.find_element(By.TAG_NAME, "details").click()  # Abre el desplegable
-        self.selenium.find_element(By.NAME, "pub_date").send_keys("2023-11-01")
+        self.selenium.find_element(By.NAME, "pub_date_0").send_keys("2023-11-01")
         
         # Añadir 100 Choices utilizando el bucle
         for i in range(100):
